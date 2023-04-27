07f2f503d0c9c237ae556ed1b88bb014c1a47459
kimdonguk-kmu <alstjs5957@gmail.com>
2023-04-27 21:21:17 +0900

    김수한무의 좌우 움직임 설정

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ fn main() {
     .add_startup_system(spawn_plane)
     .add_startup_system(spawn_object)
     .add_system(player_movement)
+    .add_system(object_movement)
     .add_system(confine_player_movement)
 
     .run();    
@@ -150,7 +151,11 @@ pub fn confine_player_movement(
 #[derive(Component)]
 pub struct Object{
     pub name: String,
-    pub velocity: f64,
+}
+
+#[derive(Component)]
+pub struct Velocity{
+    pub speed: Vec3,
 }
 
 pub fn spawn_object(
@@ -172,8 +177,54 @@ pub fn spawn_object(
             },
             Object{
                 name: "kimsuhanmu".to_string(),
-                velocity: 3.0,
+            },
+            Velocity{
+                speed: Vec3::new(1.0, 0.0, 0.0)
             },
         )
     );
+}
+
+pub fn object_movement(
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    mut object_query: Query<(&mut Velocity, &mut Transform),  With<Object>>,
+    time: Res<Time>,
+){
+    let (mut velocity, mut transform) = object_query.single_mut();
+    let mut direction = Vec3::ZERO;
+    let window: &Window = window_query.get_single().unwrap(); 
+
+    // if keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::A){
+    //     direction += Vec3::new(-1.0, 0.0, 0.0);
+    // }
+    // if keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D){
+    //     direction += Vec3::new(1.0, 0.0, 0.0);
+    // }
+    // if keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W){
+    //     direction += Vec3::new(0.0, 1.0, 0.0);
+    // }
+    // if keyboard_input.pressed(KeyCode::Down) || keyboard_input.pressed(KeyCode::S){
+    //     direction += Vec3::new(0.0, -1.0, 0.0);
+    // }
+    let x_min = 0.0;
+    let x_max = window.width();
+    direction += velocity.speed;
+
+    if direction.length() > 0.0{
+        direction = direction.normalize();
+    }
+    transform.translation += direction * OBJECT_SPEED * time.delta_seconds();
+
+    let mut translation = transform.translation;
+    if translation.x < x_min {
+        translation.x = x_min;
+        velocity.speed.x *= -1.0;
+    }
+    else if translation.x > x_max {
+        translation.x = x_max;
+        velocity.speed.x *= -1.0;
+    }
+
+    transform.translation = translation;
+    
 }
