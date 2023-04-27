87cc49dafda9ee9b206cae5a01ad25442d965dbb
Pilmo <pilmokim99@gmail.com>
2023-04-27 13:07:34 +0900

    add player movement but not confined

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ fn main() {
     .add_startup_system(spawn_player)
     .add_startup_system(spawn_camera)
     .add_startup_system(spawn_plane)
+    .add_system(player_movement)
     .run();    
 }
 
@@ -31,6 +32,36 @@ pub fn spawn_player(
             Player{},
         )
     );
+}
+
+pub const PLAYER_SPEED: f32 = 500.0;
+
+pub fn player_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut player_query: Query<&mut Transform,  With<Player>>,
+    time: Res<Time>,
+){
+    if let Ok(mut transform) = player_query.get_single_mut(){
+        let mut direction = Vec3::ZERO;
+
+        if keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::A){
+            direction += Vec3::new(-1.0, 0.0, 0.0);
+        }
+        if keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D){
+            direction += Vec3::new(1.0, 0.0, 0.0);
+        }
+        if keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W){
+            direction += Vec3::new(0.0, 1.0, 0.0);
+        }
+        if keyboard_input.pressed(KeyCode::Down) || keyboard_input.pressed(KeyCode::S){
+            direction += Vec3::new(0.0, -1.0, 0.0);
+        }
+
+        if direction.length() > 0.0{
+            direction = direction.normalize();
+        }
+        transform.translation += direction * PLAYER_SPEED * time.delta_seconds();
+    }
 }
 
 pub fn spawn_plane(
