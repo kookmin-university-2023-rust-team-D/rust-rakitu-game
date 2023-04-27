82e722d3fe97bcee81a70538d2c82c186c61f64c
Pilmo <pilmokim99@gmail.com>
2023-04-27 12:50:45 +0900

    add plane ui

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use bevy::{prelude::*, render::view::window, window::PrimaryWindow};
 
+const PLANE_X: f32 = 80.0;
+const PLANE_SIZE: Vec3 = Vec3::new(PLANE_X, 3.0, 0.0);
+
 fn main() {
     App::new()
     .add_plugins(DefaultPlugins) // 기본적인 설정을 해줍니다. 이것만 있으면 검은색 공간이 appear
     .add_startup_system(spawn_player)
     .add_startup_system(spawn_camera)
+    .add_startup_system(spawn_plane)
     .run();    
 }
 
@@ -25,6 +29,27 @@ pub fn spawn_player(
                 ..default()
             },
             Player{},
+        )
+    );
+}
+
+pub fn spawn_plane(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    assert_server: Res<AssetServer>,
+){
+    let window: &Window = window_query.get_single().unwrap();
+    commands.spawn(
+        (
+            SpriteBundle{
+                transform: Transform{
+                    translation: Vec3::new(window.height() / 2.0 , window.height() / 4.0, 0.0),
+                    scale: PLANE_SIZE,
+                    ..default()
+                } ,// z component doesn't matter in 2D game
+                texture: assert_server.load("sprites/Tiles/tile_0002.png"),
+                ..default()
+            },
         )
     );
 }
