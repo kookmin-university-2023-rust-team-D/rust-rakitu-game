0665a823bfb0e331a761d243caac9eb0b89b860d
kimdonguk-kmu <alstjs5957@gmail.com>
2023-04-30 20:20:24 +0900

    게임 오버 로직과 파일입출력 시스템 구현

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use bevy::{prelude::*, window::PrimaryWindow};
 use rust_rakitu_game::{PLANE_SIZE, PLANE, PLAYER_SIZE};
+use std::fs::File;
+use std::io::prelude::*;
 
 use bevy_matchbox::prelude::*;
 //use bevy::{prelude::*, render::camera::ScalingMode, tasks::IoTaskPool};
@@ -29,7 +31,6 @@ pub struct FrameCount {
 }
 
 fn main() {
-    
     // App::new();
     let mut app = App::new();
     GGRSPlugin::<GgrsConfig>::new()
@@ -42,21 +43,23 @@ fn main() {
     app
     .add_plugins(DefaultPlugins.set(WindowPlugin {
         primary_window: Some(Window {
-            title: "Final Project Team B".to_string(),
+            title: "Final Project Team D".to_string(),
             ..default()
         }),
         ..default()
     }))
     //.add_plugins(DefaultPlugins)
     .insert_resource(FrameCount { frame: 0 })
-    .insert_resource(GameState { is_game_over: false})
+    .insert_resource(GameState { is_game_over: false, score: 0, hp: 2})
     .add_startup_system(spawn_camera)
     .add_startup_system(spawn_plane)
     .add_startup_systems((spawn_player, start_matchbox_socket))
+    .add_startup_system(spawn_text)
     .add_systems((wait_for_players, player_movement.in_schedule(GGRSSchedule)))
     .add_system(turtle_movement)
     .add_system(turtle_hit_player)
     .add_system(game_end_system)
+    .add_system(set_hp_score)
     .run();   
 }
 
@@ -84,6 +87,9 @@ fn input(_: In<ggrs::PlayerHandle>, keys: Res<Input<KeyCode>>) -> u8 {
 
     input
 }
+
+
+
 
 pub struct GgrsConfig;
 
@@ -160,6 +166,56 @@ pub fn spawn_plane(
     );
 }
 
+pub fn spawn_text(
+    //window_query: Query<&Window, With<PrimaryWindow>>,
+    mut commands: Commands,
+    assert_server: Res<AssetServer>,
+){
+    //let window = window_query.get_single().unwrap();
+    commands.spawn(
+        TextBundle::from_sections([
+            TextSection::new(
+                "hp: ",
+                TextStyle {
+                    font: assert_server.load("sprites/NotoSansKR-Regular.otf"),
+                    font_size: 50.0,
+                    color: Color::BLACK,
+                },
+            ),
+            TextSection::from_style(TextStyle {
+                font: assert_server.load("sprites/NotoSansKR-Regular.otf"),
+                font_size: 50.0,
+                color: Color::BLACK,
+            }),
+        ])
+            .with_style(Style {
+                position_type: PositionType::Absolute,
+                position: UiRect {
+                    top: Val::Px(30.0),
+                    left: Val::Px(10.0),
+                    ..default()
+                },
+                ..default()
+            }),
+    );
+}
+
+pub fn set_hp_score(
+    mut text_query: Query<&mut Text>,
+    gamestate: ResMut<GameState>,
+){
+    
+    for (i, mut text) in text_query.iter_mut().enumerate(){
+        if i == 0{
+            text.sections[1].value = format!("{}", gamestate.hp);
+        }else{
+            text.sections[1].value = 0.to_string();
+        }
+    }
+        
+}
+
+
 pub fn spawn_camera(
     mut commands: Commands,
     window_query: Query<&Window, With<PrimaryWindow>>,
@@ -183,7 +239,6 @@ pub struct Player{
     pub is_enemy: bool,
     pub hp: i32,
     pub handle: usize,
-    pub score: i32
 }
 
 #[derive(Component)]
@@ -198,6 +253,8 @@ pub struct Velocity{
 #[derive(Resource)]
 pub struct GameState{
     pub is_game_over: bool,
+    pub score: i32,
+    pub hp: i32
 }
 
 
@@ -205,7 +262,7 @@ pub fn turtle_movement(
     mut commands: Commands,
     //window_query: Query<&Window, With<PrimaryWindow>>,
     mut turtle_query: Query<(Entity, &mut Velocity, &mut Transform),  With<Turtle>>,
-    mut player_query: Query<&mut Player>,
+    mut gamestate: ResMut<GameState>,
     time: Res<Time>,
 ){
     for (turtle, velocity, mut transform) in turtle_query.iter_mut(){
@@ -224,9 +281,7 @@ pub fn turtle_movement(
 
         let translation = transform.translation;
         if translation.y < y_min {
-            for mut player in player_query.iter_mut(){
-                player.score += 1;
-            }
+            gamestate.score += 1;
             commands.entity(turtle).despawn();
         }
 
@@ -240,14 +295,15 @@ pub fn turtle_movement(
 pub fn turtle_hit_player(
     mut commands: Commands,
     //mut game_over_event_writer: EventWriter<GameOver>,
-    mut player_query: Query<(Entity, &mut Player, &Transform), With<Rollback>>,
-    mut gameover: ResMut<GameState>,
+    mut player_query: Query<(Entity, &Player, &Transform), With<Rollback>>,
+    mut gamestate: ResMut<GameState>,
     enemy_query: Query<(Entity, &Transform), With<Turtle>>,
+    frame_count: ResMut<FrameCount>
     //asset_server: Res<AssetServer>,
     //audio: Res<Audio>,
     //score: Res<Score>,
 ) {
-    for (player_entity, mut player, player_transform) in  player_query.iter_mut() {
+    for (player_entity, player, player_transform) in  player_query.iter_mut() {
         for (turtle_entity, enemy_transform) in enemy_query.iter() {
             let distance = player_transform
                 .translation
@@ -259,14 +315,16 @@ pub fn turtle_hit_player(
                 //let sound_effect = asset_server.load("audio/explosionCrunch_000.ogg");
                 //audio.play(sound_effect);
                 commands.entity(turtle_entity).despawn();
-                player.hp -= 1;
-                if player.hp <= 0 {
-                    gameover.is_game_over = true;
-                    println!("{}", gameover.is_game_over);
-                    
-                    commands.entity(player_entity).despawn();
+                gamestate.hp -= 1;
+                if gamestate.hp <= 0 {
+                    gamestate.is_game_over = true;
+                    println!("{}", gamestate.is_game_over);
+                    if !(player.is_enemy){
+                        commands.entity(player_entity).despawn();
+                    }
                     println!("Enemy hit player! Game Over!");
-                    println!("Score: {}", player.score);
+                    println!("Score: {}", gamestate.score);
+                    println!("Time: {}", frame_count.frame / 60);
                 }
                 //game_over_event_writer.send(GameOver { score: score.value });
             }
@@ -280,13 +338,15 @@ pub fn player_movement(
     assert_server: Res<AssetServer>,
     inputs: Res<PlayerInputs<GgrsConfig>>,
     mut player_query: Query<(&Player, &mut Transform), With<Rollback>>,
-    gameover: ResMut<GameState>,
+    gamestate: ResMut<GameState>,
     mut frame_count: ResMut<FrameCount>,
     //time: Res<Time>,
-){
-    frame_count.frame += 1;
+){  
+    if !(gamestate.is_game_over){
+        frame_count.frame += 1;
+    }
     for (player, mut transform) in player_query.iter_mut(){
-        if !(gameover.is_game_over){
+        if !(gamestate.is_game_over){
             let mut direction = Vec2::ZERO;
 
         let (input, _) = inputs[player.handle];
@@ -352,7 +412,6 @@ pub fn spawn_player(
                 is_enemy: false,
                 hp: 2,
                 handle: 0,
-                score: 0
             },
             rip.next(),
             SpriteBundle{
@@ -374,7 +433,6 @@ pub fn spawn_player(
                 is_enemy: true,
                 hp: 2,
                 handle: 1,
-                score: 0
             },
             rip.next(),
             SpriteBundle{
@@ -392,8 +450,9 @@ pub fn spawn_player(
 pub fn game_end_system(
     mut commands: Commands,
     focused_windows: Query<(Entity, &Window)>,
-    gameover: ResMut<GameState>,
+    gameover: Res<GameState>,
     input: Res<Input<KeyCode>>,
+    frame_count: ResMut<FrameCount>
 ){
     for (window, focus) in focused_windows.iter() {
         if !focus.focused {
@@ -401,8 +460,20 @@ pub fn game_end_system(
         }
 
         if gameover.is_game_over && input.just_pressed(KeyCode::Q) {
+            write_file(&gameover, &frame_count);
             commands.entity(window).despawn();
         }
         
     }
+}
+
+pub fn write_file(gameover: &GameState, frame_count: &FrameCount) -> std::io::Result<()> {
+    let mut file = File::create("player_score.txt")?;
+
+    let score = gameover.score;
+    let frame = frame_count.frame / 60;
+    let message = format!("score: {}\n time: {}", score, frame);
+    file.write_all(message.as_bytes());
+
+    Ok(())
 }
